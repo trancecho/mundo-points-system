d0c9e5744bcb0c061efc8e7f8cd080c6571c7509
necolo007 <3202272848@qq.com>
2026-02-03 22:07:57 +0800

    feat:config

--- config/config.go
+++ config/config.go
@@ -1,25 +1,13 @@
 package config
 
 import (
-	"flag"
 	"github.com/spf13/viper"
 	"log"
 )
 
 // InitConfig 初始化配置
 func InitConfig() {
-	// 分不同模式加载配置文件
-	mode := flag.String("mode", "dev", "运行模式")
-	flag.Parse()
-	if *mode == "prod" {
-		viper.SetConfigName("config.prod")
-	} else if *mode == "docker" {
-		viper.SetConfigName("config.docker")
-	} else if *mode == "dev" {
-		viper.SetConfigName("config.dev")
-	} else {
-		log.Fatalf("Invalid mode: %s", *mode)
-	}
+	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("config")
 	if err := viper.ReadInConfig(); err != nil {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/spf13/viper v1.20.0
 	github.com/trancecho/mundo-gateway-sdk v0.0.0-20250322141559-9198302a53ae
 	google.golang.org/grpc v1.71.0
+	google.golang.org/protobuf v1.36.4
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
 )
@@ -51,6 +52,5 @@ require (
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
-	google.golang.org/protobuf v1.36.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
